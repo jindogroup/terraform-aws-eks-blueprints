a10dc477c56017dbf87c5ea9533eb087ec237b0f
Tochukwu Nwoko <nwokotochukwu@gmail.com>
2024-08-06 20:25:19 +0100

    Add iam permissions for fluentbit
                    "logs:TagResource",
                    "logs:PutRetentionPolicy"

--- modules/kubernetes-addons/aws-for-fluentbit/data.tf
+++ modules/kubernetes-addons/aws-for-fluentbit/data.tf
@@ -20,6 +20,8 @@ data "aws_iam_policy_document" "irsa" {
       "logs:CreateLogStream",
       "logs:DescribeLogGroups",
       "logs:DescribeLogStreams",
+      "logs:TagResource",
+      "logs:PutRetentionPolicy",
     ]
   }
 }
